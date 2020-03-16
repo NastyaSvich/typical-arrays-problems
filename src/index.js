3f8e77314e1b7fa3f83826fc7691584a8bbf88a1
NastyaSvich <56764613+NastyaSvich@users.noreply.github.com>
2020-03-17 00:04:00 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+exports.min = function min (array) {
+  if (!array || array.length == 0){return 0;}
+  else{
+return Math.min.apply(null, array);}
+}
+
+exports.max = function max (array) {
+  if (!array || array.length == 0){return 0;}
+  else{
+return Math.max.apply(null, array);}
+}
+
+exports.avg = function avg (array) {
+  if (!array || array.length == 0){return 0;}
+  else{
+return array.reduce((a, b) => (a + b)) / array.length;}
+}
